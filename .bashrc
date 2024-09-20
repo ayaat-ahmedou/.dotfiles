afccb0599b3c0300f57fb158535530e800f4e36a
Ayaat Ahmedou <ayaat.ahmedou@mths.ca>
2024-09-20 13:46:51 +0000

    added nvim

--- .bashrc
+++ .bashrc
@@ -6,15 +6,13 @@
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
       }
       
-#
     PS1='\[\e[38;5;135m\]ayaat-ahmedou\[\e[38;5;27m\]\[\033[33m\]$(parse_git_branch) ⇒ \[\e[0m\]'
-#  	
+  	
     PROMPT_DIRTRIM=1 #Just show the current directory name
     alias ls='ls --color=auto'
     alias ll='ls -alF'
     alias la='ls -A'
     alias l='ls -CF'
-
 
 # bun
 export BUN_INSTALL="$HOME/.bun"
---
+++ .config/nvim/init.lua
@@ -0,0 +1,19 @@
+-- luacheck: ignore vim
+ vim.cmd("set encoding=utf-8")
+ vim.cmd("set termguicolors")
+
+ vim.cmd("set number")
+ vim.cmd("set relativenumber")
+ vim.cmd("set cursorline")
+ vim.cmd("set numberwidth=4")
+ vim.cmd("set tabstop=2")
+ vim.cmd("set expandtab")
+ vim.cmd("set smartindent")
+ vim.cmd("set colorcolumn=80")
+
+ vim.cmd("set scrolloff=6")
+ vim.cmd("set wrap!")
+ vim.cmd("set belloff=all")
+
+ vim.cmd("set listchars=space:·,eol:↵")
+ vim.cmd("set list")
