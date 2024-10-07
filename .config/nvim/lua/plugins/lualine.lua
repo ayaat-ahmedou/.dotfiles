cdfbe7a25ac8129c575cff17d3c5cbc81f4a4a91
Ayaat Ahmedou <159163759+ayaat-ahmedou@users.noreply.github.com>
2024-10-07 04:49:35 -0400

    Added lualine

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+--luacheck: ignore vim
+return {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "horizon"
+            },
+        })
+    end
+}
