8e8158fa34bdaf65fcce9bef6cf6967deaa865b8
Ayaat Ahmedou <159163759+ayaat-ahmedou@users.noreply.github.com>
2024-10-07 04:56:56 -0400

    Added rquire user.plugins to init.lua file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,7 @@
 --luacheck: ignore vim
 require "user.options"
 require "user.keymaps"
-
-require "user.options"
-require "user.keymaps"
+require "user.plugins"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
