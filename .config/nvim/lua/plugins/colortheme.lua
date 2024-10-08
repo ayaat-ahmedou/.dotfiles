23e3b01a2a7ff64f6060b6b489c9693da9fd86c4
Ayaat Ahmedou <ayaat.ahmedou@mths.ca>
2024-10-08 14:05:17 +0000

    Fixed some set-up things

--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -1,10 +1,10 @@
 -- luacheck: ignore vim
 return {
-    "project0n/github-nvim-theme",
+    "projekt0n/github-nvim-theme",
     lazy = false, --make sure we load this during the startup if it is in your main
     priority = 1000, --make sure to load this before all the other start plug
     config = function()
         require("github-theme").setup({})
-        vim.cmd('colorscheme gitub_dark_default')
+        vim.cmd('colorscheme github_dark_default')
     end
 }
