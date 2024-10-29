ba83ac09c5ed272f97646b75d321dc33f4427a3f
Ayaat Ahmedou <159163759+ayaat-ahmedou@users.noreply.github.com>
2024-10-29 09:09:37 -0400

    Added telescope.lua

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+-- luacheck: ignore vim
+return {
+    {
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require("telescope").setup({
+          extensions = {
+            ["ui-select"] = {
+              require("telescope.themes").get_dropdown({}),
+            },
+          },
+        })
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+        vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+  
+        require("telescope").load_extension("ui-select")
+      end,
+    },
+  }
+  
